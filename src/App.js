f5da02bece2dd8afe8bf4def27923cdcba115896
ofriggit <ornella.friggit@disneystreaming.com>
2020-09-19 14:22:53 -0400

    Set up CompanyIcon component

--- src/App.js
+++ src/App.js
@@ -2,23 +2,32 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function CompanyIcon (props) {
+    return (
+    <div className="company-widget">
+        <img src={props.imageSource} alt={props.name + " logo"} />
+        <h3>{props.name}</h3>
+        <h4>Rating: {props.rating}</h4>
+    </div>
+    );
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <h1>Vibe Check</h1>
+          <nav>
+            <a href="/">About Us</a>
+          </nav>
+          <p>Tagline</p>
       </header>
+          <div className="icon-group">
+            <CompanyIcon imageSource={logo} name="Facebook" rating="B" />
+            <CompanyIcon imageSource={logo} name="Airbnb" rating="A+" />
+            <CompanyIcon imageSource={logo} name="Nike" rating="A-" />
+            <CompanyIcon imageSource={logo} name="Old Navy" rating="B+" />
+          </div>
     </div>
   );
 }
