8c929359a97c8586946fe9fe0bef4e82beb4f3b1
ofriggit <ornella.friggit@disneystreaming.com>
2020-09-19 20:54:56 -0400

    Successfully mapped companies to CompanyIcons

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function CompanyIcon (props) {
     return (
     <div className="company-widget">
-        <img src={props.imageSource} alt={props.name + " logo"} />
+        <img className="company-logo" src={props.imageSource} alt={props.name + " logo"} />
         <h3>{props.name}</h3>
         <h4>Rating: {props.rating}</h4>
     </div>
@@ -42,10 +42,7 @@ class App extends React.Component {
                 <p>Tagline</p>
             </header>
                 <div className="icon-group">
-                  <CompanyIcon imageSource={logo} name="Facebook" rating="B" />
-                  <CompanyIcon imageSource={logo} name="Airbnb" rating="A+" />
-                  <CompanyIcon imageSource={logo} name="Nike" rating="A-" />
-                  <CompanyIcon imageSource={logo} name="Old Navy" rating="B+" />
+                    {this.state.companiesData.map(company => <CompanyIcon imageSource={company.logo} name={company.name} />)}
                 </div>
           </div>
         );
