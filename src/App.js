345b3b9d598119cdd6b203228fad60ae6151145e
ofriggit <ornella.friggit@disneystreaming.com>
2020-09-19 20:40:18 -0400

    Successfully fetched API Data

--- src/App.js
+++ src/App.js
@@ -12,24 +12,44 @@ function CompanyIcon (props) {
     );
 }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-          <h1>Vibe Check</h1>
-          <nav>
-            <a href="/">About Us</a>
-          </nav>
-          <p>Tagline</p>
-      </header>
-          <div className="icon-group">
-            <CompanyIcon imageSource={logo} name="Facebook" rating="B" />
-            <CompanyIcon imageSource={logo} name="Airbnb" rating="A+" />
-            <CompanyIcon imageSource={logo} name="Nike" rating="A-" />
-            <CompanyIcon imageSource={logo} name="Old Navy" rating="B+" />
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            companiesData: [],
+            error: null
+        }
+    }
+
+    componentDidMount () {
+        fetch('https://vibe-check-companies.herokuapp.com/companies')
+        .then(res => res.json())
+        .then(result => {
+            this.setState({ companiesData: result })
+        },
+        error => this.setState({ error }));
+    }
+
+    render() {
+        return (
+          <div className="App">
+            <header className="App-header">
+                <h1>Vibe Check</h1>
+                <nav>
+                  <a href="/">About Us</a>
+                </nav>
+                <p>Tagline</p>
+            </header>
+                <div className="icon-group">
+                  <CompanyIcon imageSource={logo} name="Facebook" rating="B" />
+                  <CompanyIcon imageSource={logo} name="Airbnb" rating="A+" />
+                  <CompanyIcon imageSource={logo} name="Nike" rating="A-" />
+                  <CompanyIcon imageSource={logo} name="Old Navy" rating="B+" />
+                </div>
           </div>
-    </div>
-  );
+        );
+    }
 }
 
 export default App;
