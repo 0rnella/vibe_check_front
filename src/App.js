981b69369eaa5e8a707fc18cd4e3b36cafb3b3f1
ofriggit <ornella.friggit@disneystreaming.com>
2020-09-28 18:21:18 -0400

    Stricter linting

--- src/App.js
+++ src/App.js
@@ -1,52 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function CompanyIcon (props) {
-    return (
+  return (
     <div className="company-widget">
-        <img className="company-logo" src={props.imageSource} alt={props.name + " logo"} />
-        <h3>{props.name}</h3>
-        <h4>Rating: {props.rating}</h4>
+      <img className="company-logo" src={props.imageSource} alt={props.name + " logo"} />
+      <h3>{props.name}</h3>
+      <h4>Rating: {props.rating}</h4>
     </div>
-    );
+  );
 }
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            companiesData: [],
-            error: null
-        }
-    }
+    this.state = {
+      companiesData: [],
+      error: null
+    };
+  }
 
-    componentDidMount () {
-        fetch('https://vibe-check-companies.herokuapp.com/companies')
-        .then(res => res.json())
-        .then(result => {
-            this.setState({ companiesData: result })
-        },
-        error => this.setState({ error }));
-    }
+  componentDidMount () {
+    fetch('https://vibe-check-companies.herokuapp.com/companies')
+      .then(res => res.json())
+      .then(result => {
+        this.setState({ companiesData: result });
+      },
+      error => this.setState({ error }));
+  }
 
-    render() {
-        return (
-          <div className="App">
-            <header className="App-header">
-                <h1>Vibe Check</h1>
-                <nav>
-                  <a href="/">About Us</a>
-                </nav>
-                <p>Tagline</p>
-            </header>
-                <div className="icon-group">
-                    {this.state.companiesData.map(company => <CompanyIcon imageSource={company.logo} name={company.name} />)}
-                </div>
-          </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Vibe Check</h1>
+          <nav>
+            <a href="/">About Us</a>
+          </nav>
+          <p>Tagline</p>
+        </header>
+        <div className="icon-group">
+          {this.state.companiesData.map(company => {
+            const { logo, name } = company;
+            return <CompanyIcon imageSource={logo} name={name} />;
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
