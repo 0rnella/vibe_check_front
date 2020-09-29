1a7dbadafe327f23d2a6017eeb5e559e54992b2f
ofriggit <ornella.friggit@disneystreaming.com>
2020-09-29 19:44:19 -0400

    Successfully created about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,27 @@
 import React from 'react';
+import aboutData from '../about-page.json';
+
+function GroupMember (props) {
+  return (
+    <div className='group-member'>
+      <h3>{props.name}</h3>
+      <img className='member-pic' alt={props.name} src={props.image} />
+      <p>{props.bio}</p>
+      <a href={props.link} >Get in touch</a>
+    </div>
+  );
+}
 
 function AboutPage (props) {
   return (
     <main>
       <div className='intro' >
-        <p>Intro paragraph</p>
+        <p>{aboutData.intro}</p>
       </div>
-      <div className='group-member'>
-        <img className='member-pic' />
-        <p>Person's description</p>
-      </div>
+      {aboutData.groupMembers.map(member => {
+        const { bio, name, image, link } = member;
+        return <GroupMember bio={bio} name={name} image={image} link={link} />;
+      })}
     </main>
   );
 }
