64c32aed53352fd6f3633d42f33a7743b6c2434f
ofriggit <ornella.friggit@disneystreaming.com>
2021-01-18 13:27:20 -0500

    Move Latinx out of race

--- src/components/DemographicsCategory.js
+++ src/components/DemographicsCategory.js
@@ -12,9 +12,13 @@ const DemographicsCategory = (props) => {
       white: demoData.white,
       black: demoData.black,
       asian: demoData.asian,
-      latinx: demoData.latinx,
       indigenous: demoData.indigenous,
       multiracial: demoData.multiracial,
+    };
+    
+    const ethnicity = {
+      latinx: demoData.latinx,
+      'non-latinx': 1- demoData.latinx
     };
 
     const gender = {
@@ -25,17 +29,21 @@ const DemographicsCategory = (props) => {
     return {
       race: getPieChartData(race),
       gender: getPieChartData(gender),
+      ethnicity: getPieChartData(ethnicity),
       population: demoData.employeePopulation
     };
   });
 
-  const overallRace = {
+  const overall = {
     white: 0.763,
     black: 0.134,
     asian: 0.059,
     latinx: 0.185,
+    'non-latinx': 0.815,
     indigenous: 0.015,
-    multiracial: 0.028
+    multiracial: 0.028,
+    women: 0.47,
+    men: 0.53
   };
 
   const raceColorScale = ["#D8A48F", "#4D4D3F", "#EFEBCE", "#A3A380", "#6F493D"];
@@ -58,17 +66,12 @@ const DemographicsCategory = (props) => {
                 'We were not able to find data.'}
             </div>
 
-            {data.race.length && <div className='item'>
-              <p>Compared to the overall US population:</p>
-              <ul>
-                {data.race.map(raceObj => {
-                  const { x: raceName, y: percentage } = raceObj;
-                  const difference = Math.round(100*(percentage - overallRace[raceName]));
-
-                  return <li key={raceName}>There are {difference > 0 ? `${difference}% more` : `${-difference}% fewer`} {raceName} employees</li>;
-                })}
-              </ul>
-            </div>}
+            <div class='item'>
+              <h5>Ethnicity</h5>
+              {data.ethnicity.length ? 
+                <VictoryPie data={data.ethnicity} padding={70} /> :
+                'We were not able to find data.'}
+            </div>
 
             <div className='item'>
               <h5>Gender</h5>
@@ -76,6 +79,32 @@ const DemographicsCategory = (props) => {
                 <VictoryPie data={data.gender} colorScale={genderColorScale} padding={70} /> :
                 'We were not able to find data.'}
             </div>
+
+            {<div className='item'>
+              <p>Compared to the overall US population:</p>
+              <ul>
+                {data.race.length && data.race.map(raceObj => {
+                  const { x: raceName, y: percentage } = raceObj;
+                  const difference = Math.round(100*(percentage - overall[raceName]));
+
+                  return <li key={raceName}>There are {difference > 0 ? `${difference}% more` : `${-difference}% fewer`} {raceName} employees</li>;
+                })}
+                {data.ethnicity.length && data.ethnicity.map(ethnicityObj => {
+                  const { x: ethnicity, y: percentage } = ethnicityObj;
+                  const difference = Math.round(100*(percentage - overall[ethnicity]));
+
+                  return <li key={ethnicity}>There are {difference > 0 ? `${difference}% more` : `${-difference}% fewer`} {ethnicity} employees</li>;
+                })}
+                {data.gender.length && data.gender.map(genderObj => {
+                  const { x: gender, y: percentage } = genderObj;
+                  const difference = Math.round(100*(percentage - overall[gender]));
+
+                  return <li key={gender}>There are {difference > 0 ? `${difference}% more` : `${-difference}% fewer`} {gender} employees</li>;
+                })}
+              </ul>
+            </div>}
+
+            
           </div>
 
         </Fragment>
