cb403d25e50cc1e62ace31fdf51cdf186c5d03c8
ofriggit <ornella.friggit@disneystreaming.com>
2021-01-25 17:47:49 -0500

    feat(footer): add feedback form

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import Nav from './Nav';
 
 function Footer (props) {
   return(<footer className='footer'>
+    <p><b><a href='https://airtable.com/shreyDhU6fd3mwhAm' target='_blank' rel='noopener noreferrer' >Give Feedback!</a></b></p>
     <Nav />
     <p>Made with love, last updated January 2021</p>
   </footer>);
